9917d7dd5703b26e0d2700c35e5b7d4db3ca904f
БЕЗДОТНая Хоопс ✶ ДоскаИгры <vnifedow@bk.ru>
2022-03-13 16:23:08 +0500

    1

--- dev-front/main.js
+++ dev-front/main.js
@@ -114,7 +114,7 @@ async function showAvailable() {
     let list = await axios.get(server + `api/serverList`);
     canvas.innerHTML += uploadTemplate();
     if (list.data.length == 0) {
-        canvas.innerHTML += "<h2 style='text-align:center;'>Нет доступных сборок</h2>";
+        canvas.innerHTML += "<h2 style='text-align:center; id='na'>Нет доступных сборок</h2>";
     }
     for (let i = 0; i < list.data.length; i++) {
         let image = await axios.get(server + `api/serverIcon/${
@@ -126,7 +126,6 @@ async function showAvailable() {
 }
 
 async function uploadServer() {
-    canvas.innerHTML += "<h2 style='text-align:center;'>Падажжи, обрабатываю...</h2>";
     let fd = new FormData();
     let sfile = document.querySelector("#sfile");
     let pfile = document.querySelector("#spic");
@@ -139,6 +138,10 @@ async function uploadServer() {
             'Content-Type': 'multipart/form-data'
         }
     });
+    if(document.querySelector('#na') != null) {
+        canvas.innerHTML = '';
+    }
+    canvas.innerHTML += "<h2 style='text-align:center;'>Падажжи, обрабатываю...</h2>";
     await uploadModifications(name, 'plugin');
     res.data == 'OK' ? show('packages') : console.log("Error");
 }
